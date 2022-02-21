1122f609585f345ba6f7f8b6a8c0907c90a51dca
Matrix <1337816495@qq.com>
2022-02-21 23:43:30 +0800

    u

--- source/shader/fragment_shader.glsl
+++ source/shader/fragment_shader.glsl
@@ -2,14 +2,18 @@
 precision mediump float;
 #endif
 
+#define PI 3.14159265359
+
 uniform vec2 u_resolution;
 uniform vec2 u_mouse;
 uniform float u_time;
 
-/* Plot a line on Y using a value between 0.0~1.0 */
-float plot( vec2 st ) {
+float plot( vec2 st, float pct ) {
 
-    return smoothstep( 0.02, 0.0, abs( st.y - st.x ) );
+    return (
+        smoothstep( pct - 0.02, pct, st.y ) -
+        smoothstep( pct, pct + 0.02, st.y )
+    );
 
 }
 
@@ -17,12 +21,12 @@ void main() {
 
     vec2 st = gl_FragCoord.xy / u_resolution;
 
-    float y = st.x;
+    float y = mod( st.x, 0.5 );
 
     vec3 color = vec3( y );
 
-    /* Plot a line. */
-    float pct = plot( st );
+
+    float pct = plot( st, y );
 
     color = ( 1.0 - pct ) * color + pct * vec3( 0.0, 1.0, 0.0 );
 
